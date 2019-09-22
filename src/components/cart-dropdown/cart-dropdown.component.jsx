c8376dc593b7ccbda52545aab630535e206df193
kevinvai10 <kevin.cbdev@gmail.com>
2019-09-21 23:15:42 -0700

    added checkout page and added remove and clear item reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,27 +1,41 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import CustomButton from '../custom-button/custom-button.component'
-import CartItem from '../cart-item/cart-item.component'
-import { selectCartItems } from '../../redux/cart/cart.selectors'
-import './cart-dropdown.styles.scss'
+import React from 'react';
+import { connect } from 'react-redux';
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { withRouter } from 'react-router-dom';
+import './cart-dropdown.styles.scss';
 
-
-const CartDropDown = ({cartItems}) => (
-    <div className='cart-dropdown'>
-        <div className='cart-items'>
-            {
-                cartItems.map(cartItem => <CartItem key={cartItem.id} item={ cartItem } />)
-            }
-        </div>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+const CartDropDown = ({ cartItems, history, dispatch }) => (
+  <div className='cart-dropdown'>
+    <div className='cart-items'>
+      {cartItems.length ? (
+        cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className='empty-message'>Your cart is empty</span>
+      )}
     </div>
-)
+    <CustomButton onClick={() => {
+      history.push('/checkout');
+      dispatch(toggleCartHidden())
+      }}>
+      GO TO CHECKOUT
+    </CustomButton>
+  </div>
+);
 /*const mapStateToProps = ({cart: { cartItems } }) => ({
     cartItems
 })*/
 
 const mapStateToProps = state => ({
-    cartItems: selectCartItems(state)
-})
+  cartItems: selectCartItems(state)
+});
 
-export default connect(mapStateToProps)(CartDropDown)
+/*
+we recieve dispatch by default from connect whenever dispatch is not sent as a param
+ */
+
+export default withRouter(connect(mapStateToProps)(CartDropDown));
---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.actions'
+import './checkout-item.styles.scss'
+
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+    const { name, price, quantity, imageUrl } = cartItem;
+    return (
+        <div className='checkout-item'>
+            <div className='image-container'>
+                <img src={imageUrl} alt='product'/>
+            </div>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</div>    
+                    <span className='value'>{quantity}</span>
+                <div className='arrow' onClick={() => addItem(cartItem)}>&#10095;</div>    
+            </span>
+            <div className='remove-button' onClick={ () => clearItem(cartItem) }>
+                &#10005;
+            </div>
+        </div>
+    )
+}
+
+const MapDispatchToProps = dispatch => ({
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item)),
+})
+
+export default connect(null, MapDispatchToProps)(CheckoutItem)
